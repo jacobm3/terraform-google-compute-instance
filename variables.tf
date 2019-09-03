f7b518257f7028968ac54cc919edca5415464e9c
Jacob <jacobm3@gmail.com>
2019-09-02 23:41:48 -0500

    Update webroot content

--- variables.tf
+++ variables.tf
@@ -24,8 +24,12 @@ variable "startup_script" {
 apt-get update
 apt-get install -y apache2
 cat <<EOF > /var/www/html/index.html
-<html><body><h1>Hello World</h1>
+<html><body>
+<div align="center"><h1>Hello World</h1>
 <p>This page was created from a simple startup script!</p>
+<br><br><br>
+<img src="https://www.meetmindful.com/wp-content/uploads/2016/01/peace-meditate-nature-outside-night-night-fall-peace-solitude1.jpg"/>
+</div>
 </body></html>
 EOF
 EOV
